e8a1d13a573a4146ed45e7d9293236c89e50f140
scg984261 <scottgillen@hotmail.com>
2024-12-26 14:25:59 +0000

    Create scrip to import bacpac database file

---
+++ SqlPackage-Import.ps1
@@ -0,0 +1,3 @@
+$SourceFile="C:\Users\scott\dev-work\DatabaseBackups\TestDatabase.bacpac"
+$ConnectionString="Server=DESKTOP-3GIU7MF\SQLEXPRESS;initial catalog=TestDatabase;trusted_connection=true;TrustServerCertificate=True"
+& 'C:\Program Files\Microsoft SQL Server\160\DAC\bin\SqlPackage.exe' /Action:Import /SourceFile:$SourceFile /TargetConnectionString:$ConnectionString
