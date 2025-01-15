69ee4a817de1352506e6114746e99b3e349ac1a4
scg984261 <scottgillen@hotmail.com>
2025-01-15 16:41:06 +0000

    Minor change to export database and publish dacpac scripts

--- Export-Database.bat
+++ Export-Database.bat
@@ -1,1 +1,1 @@
-PowerShell.exe .\Export-Database.ps1
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\scott\dev-work\Scripts\Export-Database.ps1
--- SqlPackage-PublishDacpac.ps1
+++ SqlPackage-PublishDacpac.ps1
@@ -4,6 +4,13 @@ Write-Host "Database name is ${DatabaseName}";
 $DacpacFile="C:\Users\scott\dev-work\CDB\bin\Debug\${DatabaseName}.dacpac";
 Write-Host "DACPAC file is ${DacpacFile}";
 
+if (Test-Path $DacpacFile) {
+	Write-Host "File found locally: ${DacpacFile}";
+} else {
+	Write-Host "DACPAC file ${DacpacFile} not found. Exiting script."
+	exit 1;
+}
+
 $ConnectionString="Server=DESKTOP-3GIU7MF\SQLEXPRESS;initial catalog=${DatabaseName};trusted_connection=true;TrustServerCertificate=True";
 Write-Host "Connection string is ${ConnectionString}";
 
