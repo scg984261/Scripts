43054ab7be91ef0a01b1974cd3d959ca2b72e0dc
scg984261 <scottgillen@hotmail.com>
2025-04-13 20:48:11 +0100

    Changes to publish dacpac script

--- SqlPackage-PublishDacpac.ps1
+++ SqlPackage-PublishDacpac.ps1
@@ -7,11 +7,16 @@ Write-Host "DACPAC file is ${DacpacFile}";
 if (Test-Path $DacpacFile) {
 	Write-Host "File found locally: ${DacpacFile}";
 } else {
-	Write-Host "DACPAC file ${DacpacFile} not found. Exiting script."
+	Write-Host "DACPAC file ${DacpacFile} not found. Exiting script.";
 	exit 1;
 }
 
-$ConnectionString="Server=DESKTOP-3GIU7MF\SQLEXPRESS;initial catalog=${DatabaseName};trusted_connection=true;TrustServerCertificate=True";
+$MachineName=$env:COMPUTERNAME
+Write-Host "Machine name is ${MachineName}";
+$ServerInstanceName="${MachineName}\SQLEXPRESS"
+Write-Host "SQL Server instance name is ${ServerInstanceName}";
+
+$ConnectionString="Server=${ServerInstanceName};initial catalog=${DatabaseName};trusted_connection=true;TrustServerCertificate=True";
 Write-Host "Connection string is ${ConnectionString}";
 
-& 'C:\Program Files\Microsoft SQL Server\160\DAC\bin\SqlPackage.exe' /Action:Publish /SourceFile:$DacpacFile /TargetConnectionString:$ConnectionString /SourceTrustServerCertificate:True
+& 'C:\Program Files\Microsoft SQL Server\160\DAC\bin\SqlPackage.exe' /Action:Publish /SourceFile:$DacpacFile /TargetConnectionString:$ConnectionString /SourceTrustServerCertificate:True /p:BlockOnPossibleDataLoss=false
